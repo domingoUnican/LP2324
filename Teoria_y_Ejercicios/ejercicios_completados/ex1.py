f2fd44a6de803045d89912436fde6410ef26a398
Carlos Fernandez Cabrero <carlos.fernandezcab@alumnos.unican.es>
2024-02-13 17:02:52 +0100

    ex1 modified

--- Teoria_y_Ejercicios/ejercicios_completados/ex1.py
+++ Teoria_y_Ejercicios/ejercicios_completados/ex1.py
@@ -2,11 +2,13 @@
 
 import re
 
+RESERVED = r'(?P<RESERVED>int|if)'
+EQUAL = r'(?P<EQUAL>=)'
 ID = r'(?P<ID>[a-zA-Z_][a-zA-Z0-9_]*)'
 NUMBER = r'(?P<NUMBER>\d+)'
 SPACE = r'(?P<SPACE>\s+)'
 
-patterns = [ID, NUMBER, SPACE]
+patterns = [RESERVED, EQUAL, ID, NUMBER, SPACE]
 
 # Make the master regex pattern
 pat = re.compile('|'.join(patterns))
@@ -15,14 +17,21 @@ def tokenize(text):
     index = 0
     while index < len(text):
         m = pat.match(text,index)
-        if m:
+
+        if m is None:
+            print('Bad character %r' % text[index])
+            index = index + 1
+        elif m.group() != ' ' :
             yield (m.lastgroup, m.group())
+            index = m.end()
+        elif m.group() == ' ':
             index = m.end()
         else:
             raise SyntaxError('Bad char %r' % text[index])
 
 # Sample usage
-text = 'abc 123 cde 456'
+text = 'abc if = 123 $ cde 456'
 
 for tok in tokenize(text):
+    #if tok != ('SPACE', ' '):
     print(tok)
