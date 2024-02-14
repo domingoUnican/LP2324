8502f569486506462ed902f51da77992531b9926
RubaGarcia <rgm177@alumnos.unican.es>
2024-02-14 17:32:20 +0100

    comenzado el lexer. 6/26

--- Practicas_Grupo/Lexer.py
+++ Practicas_Grupo/Lexer.py
@@ -15,7 +15,7 @@ class CoolLexer(Lexer):
               INHERITS, ISVOID, LET, LOOP, NEW, OF,
               POOL, THEN, WHILE, NUMBER, STR_CONST, LE, DARROW, ASSIGN}
     #ignore = '\t '
-    literals = {}
+    literals = {"=","+","-","*","/","(",")","<",">",".",";",":","@",'"','{','}'}
     # Ejemplo
     ELSE = r'\b[eE][lL][sS][eE]\b'
 
@@ -23,13 +23,129 @@ class CoolLexer(Lexer):
                           for i in ['0', '1']
                           for j in range(16)] + [bytes.fromhex(hex(127)[-2:]).decode("ascii")]
 
+
+    @_(r'\b[iI][fF]\b')
+    def IF(self,t):
+        return t
+
+    @_(r'\b[fF][iI]\b')
+    def FI(self,t):
+        return t
+        
+    @_(r'\b[tT][hH][eE][nN]')
+    def THEN(self,t):
+        return t
+    
+    @_(r'\b[nN][oO][tT]\b')
+    def NOT(self,t):
+        return t
+
+    @_(r'\b[iI][nN]\b')
+    def IN(self,t):
+        return t
+
+    @_(r'\b[cC][aA][sS][eE]\b')
+    def CASE(self, t):
+        return t
+    
+    @_(r'[Ee][Ss][Aa][Cc]')
+    def ESAC(self,t):
+        return t
+    
+    @_(r'\b[Cc][Ll][aA][Ss]{2}\b')
+    def CLASS(self,t):
+        return t
+    
+    @_(r'\b[Nn][Ee][Ww]\b')
+    def NEW(self,t):
+        return t
+    
+    @_(r'\b[Ll][Oo]{2}[Pp]\b')
+    def LOOP(self,t):
+        return t
+    
+    @_(r'\b[Ll][Ee][Tt]\b')
+    def LET(self,t):
+        return t
+
+    @_(r'\b[Ii][Ss][Vv][Oo][Ii][Dd]\b')
+    def ISVOID(self,t):
+        return t
+    
+    @_(r'\b[Ii][Nn][Hh][Ee][Rr][Ii][Tt][Ss]\b')
+    def INHERITS(self,t):
+        return t
+
+    @_(r'\b[Oo][Ff]\b')
+    def OF(self,t):
+        return t
+
+    @_(r'\b[Pp][Oo][Oo][Ll]\b')
+    def POOL(self,t):
+        return t
+    
+    @_(r'\b[Tt][Hh][Ee][Nn]\b')
+    def THEN(self,t):
+        return t
+    
+    @_(r'\b[Ww][Hh][Ii][Ll][Ee]\b')
+    def WHILE(self,t):
+        return t
+        
+    #@_(r'\bT[Rr][Uu][Ee]\b')
+    #def TRUE(self,t):
+    #    return t
+        
+    #@_(r'\bF[Aa][Ll][Ss][Ee]\b')
+    #def FALSE(self,t):
+    #    return t
+
+    @_(r'\b((t[rR][uU][eE])|(f[aA][lL][sS][eE]))\b')
+    def BOOL_CONST(self,t):
+        t.value = t.value[0] == 't'
+        return t
+    
+    @_(r'[A-Z][A-Za-z0-9_]*')
+    def TYPEID(self,t):
+        return t
+
+    @_(r'[a-z][A-Za-z0-9_]*')
+    def OBJECTID(self,t):
+        return t
+
+    @_(r'[0-9]+')
+    def INT_CONST(self,t):
+        return t
+
+    @_(r'\b<=\b')
+    def LE(self,t):
+        return t
+
+    @_(r'\b=>\b')
+    def DARROW(self,t):
+        return t
+    
+    @_(r'\b<-\b')
+    def ASSIGN(self,t):
+        return t
+    
     @_(r'\t| |\v|\r|\f')
     def spaces(self, t):
+        pass
+
+    @_(r'-{2}.*\n?$|\(\*(.|\n)*\*\)')
+    def comments(self, t):
         pass
 
     @_(r'\n+')
     def newline(self, t):
         self.lineno += t.value.count('\n')
+
+    @_(r'(["])(.{1,1024})\1')
+    def STR_CONST(self, t):
+        t.value = t.value[1:-1]
+        #completar TODO
+        
 
     
     def error(self, t):
