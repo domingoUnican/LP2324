ce0c652dedc4dc92539668c33c354f9471e3e7b5
Miguel de la Cal <mdg648@alumnos.unican.es>
2024-05-07 17:17:30 +0200

    minimos p04 terminados

--- Practicas_Grupo/Base_clases.py
+++ Practicas_Grupo/Base_clases.py
@@ -30,7 +30,7 @@ class Entero(Object):
     def __mul__(self, s):
         return Entero(self.numero * s.numero)
     
-    def __div__(self, s):
+    def __truediv__(self, s):
         return Entero(self.numero // s.numero)
 
     def __eq__(self, value: object) -> bool:
@@ -41,6 +41,9 @@ class Entero(Object):
     
     def __lt__(self, value: object) -> bool:
         return self.numero < value.numero
+    
+    def __le__(self, value: object) -> bool:
+        return self.numero <= value.numero
 
 class Cadena_carac(Object):
     def __init__(self, cadena = ""):
@@ -59,6 +62,7 @@ class Cadena_carac(Object):
     def __eq__(self, value: object) -> bool:
         return self.cadena == value
     
+        
 class Booleano(Object):
     def __init__(self, booleano = False):
         super().__init__()
@@ -92,3 +96,125 @@ class IO(Object):
 def NOT(x):
     return Booleano(not x.booleano)
 
+'''class Main(IO):
+    def __init__(self):
+      temp = Cadena_carac("2 is trivially prime.\n")
+      temp0 = temp
+      temp = self
+      temp = temp.out_string(temp0)
+      temp = Entero(2)
+
+      self.out = temp
+      temp = self.out
+      self.testee = temp
+      self.divisor = Entero(0)
+      temp = Entero(500)
+      self.stop = temp
+      temp = Booleano(True)
+      while(temp == True):
+        temp = self.testee
+        tempsuma1 = temp
+        temp = Entero(1)
+        temp = tempsuma1 + temp
+        self.testee = temp
+        temp = Entero(2)
+        self.divisor = temp
+        temp = self.testee
+        tempmenor = temp
+        temp = self.divisor
+        tempmul2 = temp
+        temp = self.divisor
+        temp = tempmul2 * temp
+        temp = tempmenor < temp
+        if True == temp:
+          temp = Booleano(False)
+        else:
+          temp = self.testee
+          tempresta3 = temp
+          temp = self.divisor
+          tempmul4 = temp
+          temp = self.testee
+          tempdiv5 = temp
+          temp = self.divisor
+          temp = tempdiv5 / temp
+          temp = tempmul4 * temp
+          temp = tempresta3 - temp
+          temp6 = temp
+          temp = Entero(0)
+          temp7 = temp
+          temp = (temp6 == temp7)
+          if True == temp:
+            temp = Booleano(False)
+          else:
+            temp = Booleano(True)
+        while(temp == True):
+          temp = self.divisor
+          tempsuma8 = temp
+          temp = Entero(1)
+          temp = tempsuma8 + temp
+          self.divisor = temp
+          temp = self.testee
+          tempmenor = temp
+          temp = self.divisor
+          tempmul9 = temp
+          temp = self.divisor
+          temp = tempmul9 * temp
+          temp = tempmenor < temp
+          if True == temp:
+            temp = Booleano(False)
+          else:
+            temp = self.testee
+            tempresta10 = temp
+            temp = self.divisor
+            tempmul11 = temp
+            temp = self.testee
+            tempdiv12 = temp
+            temp = self.divisor
+            temp = tempdiv12 / temp
+            temp = tempmul11 * temp
+            temp = tempresta10 - temp
+            temp13 = temp
+            temp = Entero(0)
+            temp14 = temp
+            temp = (temp13 == temp14)
+            if True == temp:
+              temp = Booleano(False)
+            else:
+              temp = Booleano(True)
+
+        temp = self.testee
+        tempmenor = temp
+        temp = self.divisor
+        tempmul15 = temp
+        temp = self.divisor
+        temp = tempmul15 * temp
+        temp = tempmenor < temp
+        if True == temp:
+          temp = temp = self.testee
+          self.out = temp
+          temp = self.out
+          temp16 = temp
+          temp = self
+          temp = temp.out_int(temp16)
+          temp = Cadena_carac(" is prime.\n")
+          temp17 = temp
+          temp = self
+          temp = temp.out_string(temp17)
+
+        else:
+          temp = Entero(0)
+
+        if self.stop <= self.testee:
+          Cadena_carac("halt").abort()
+        else:
+          temp = "continue"
+
+        temp = Booleano(True)
+
+      self.m = temp
+    def main(self):
+      temp = Entero(0)
+      return temp
+Main().main()
+
+'''
